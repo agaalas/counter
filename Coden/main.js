85dc3eadcb70d908f6bbfbe97b0f804f626571cf
Aga <111914860+agaalas@users.noreply.github.com>
2025-10-29 00:52:34 +0100

    Delete Coden directory

--- Coden/main.js
+++
@@ -1,9 +0,0 @@
-const button = document.getElementById("btn")
-const span = document.getElementById("count")
-let counti=0
-
-button.addEventListener("click", ()=>{
-    counti++;
-    span.textContent= `Ich liebe dich ${counti} mal`
-})
-
